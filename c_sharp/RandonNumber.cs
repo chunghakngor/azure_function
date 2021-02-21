171ab1d9b1f492b2502c01cf502d5ff1c6662da9
Chung Hak Ngor <chunghakngor@gmail.com>
2021-02-22 00:17:52 +1300

    c_sharp update, added random number and deploy

---
+++ c_sharp/RandonNumber.cs
@@ -0,0 +1,27 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Test.Function
+{
+  public static class RandonNumber
+  {
+    [FunctionName("RandonNumber")]
+    public static async Task<IActionResult> Run(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+        ILogger log)
+    {
+      log.LogInformation("[HTTP] Randon number function processed a request.");
+      Random _random = new Random();
+      var maxNumber = String.IsNullOrEmpty(req.Query["max"]) ? 100 : Int32.Parse(req.Query["max"]);
+
+      return new OkObjectResult(new { status = "ok", max = maxNumber, random = _random.Next(maxNumber) });
+    }
+  }
+}
